85c47799886de4e0f6ec9af2666afd8b78561d2f
CassioAA <cassioalmeidaccti@gmail.com>
2025-10-07 21:20:33 -0300

    add project setup and data range class

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+async def root() -> dict[str, str]:
+    return {"message": "fastapi is working"}
+@app.get("/hello/{name}")
+async def say_hello(name: str) -> dict[str, str]:
+    return {"message": f"hi {name}" }
